ccb2a1d399fb0bfba0604e7e77c53306f281b8a1
jane1002 <jane.fan1002@gmail.coom>
2020-03-09 07:11:27 -0500

    Update readme.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # reportPOC
+
+not use index2.js
+
+1. npm install
+2. npm start
--- src/index2.js
+++ src/index2.js
@@ -1,37 +1,38 @@
-var azure = require('azure');
-var storageAccount = '<Your Storage Account>';
-var accessKey = '<Your Access Key>'
-var blobSvc = azure.createBlobService(storageAccount, accessKey);
-
-var express = require('express');
-var formidable = require('formidable'); // the same node module as Azure sample
-var app = express();
-
-app.use(express.static('public'));   // contains a form page using the same code as yours
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-app.post('/upload', function(req, res) {
-  var form = new formidable.IncomingForm();
-  form.parse(req, function(err, fields, files) {
-    var options = {
-      contentType: files.myfile.type,
-      metadata: { fileName: files.myfile.name }
-    };
-    blobSvc.createBlockBlobFromLocalFile('mycontainer', files.myfile.name, files.myfile.path, options, function(error, result, response) {
-      if(!error){
-        // file uploaded
-        res.send('file uploaded!');
-      }
-    });
-  });
-  //console.log(req);
-});
-
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+// not use
+// var azure = require('azure');
+// var storageAccount = '<Your Storage Account>';
+// var accessKey = '<Your Access Key>'
+// var blobSvc = azure.createBlobService(storageAccount, accessKey);
+//
+// var express = require('express');
+// var formidable = require('formidable'); // the same node module as Azure sample
+// var app = express();
+//
+// app.use(express.static('public'));   // contains a form page using the same code as yours
+//
+// app.get('/', function(req, res) {
+//   res.send('Hello World!');
+// });
+//
+// app.post('/upload', function(req, res) {
+//   var form = new formidable.IncomingForm();
+//   form.parse(req, function(err, fields, files) {
+//     var options = {
+//       contentType: files.myfile.type,
+//       metadata: { fileName: files.myfile.name }
+//     };
+//     blobSvc.createBlockBlobFromLocalFile('mycontainer', files.myfile.name, files.myfile.path, options, function(error, result, response) {
+//       if(!error){
+//         // file uploaded
+//         res.send('file uploaded!');
+//       }
+//     });
+//   });
+//   //console.log(req);
+// });
+//
+// var server = app.listen(3000, function() {
+//   var host = server.address().address;
+//   var port = server.address().port;
+//   console.log('Example app listening at http://%s:%s', host, port);
+// });
